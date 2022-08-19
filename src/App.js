8bfa937d22eb7380b067d80f8bbc59f524f0386e
fernandasayuri <fernanda.ito@terceiro-sky.com.br>
2022-08-19 17:28:59 -0300

    iterando arrays

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    name: 'Otávio Miranda',
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      },
+    ]
   };
 
   handlePClick = () => {
@@ -19,26 +34,16 @@ class App extends Component {
   }
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* click on name to chance name state */}
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            aqui para alterar estado counter
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
